c7f6e58600e8db29a14d4c07b21ea485839742a8
Charles <charlesbreton99@gmail.com>
2022-02-14 13:24:28 -0700

    WORKING BUILD with cleaned up code

--- src/Viewer/index.js
+++ src/Viewer/index.js
@@ -3,20 +3,16 @@ import * as BABYLON from '@babylonjs/core';
 import BabylonScene from '../BabylonScene'; // import the component above linking to file we just created.
 
 
-
-
-const m4URL = 'https://raw.githubusercontent.com/TheNosiriN/Babylon-Assets/master/m4a1.obj';
 const ybotURL = 'https://raw.githubusercontent.com/TheNosiriN/Babylon-Assets/master/ybot.babylon';
-const towerURL = 'https://raw.githubusercontent.com/TheNosiriN/Babylon-Assets/master/tower%20obj/Tower-House%20Design.obj';
-const streetURL = 'https://raw.githubusercontent.com/TheNosiriN/Babylon-Assets/master/street/Street%20environment_V01.obj';
-
+// const m4URL = 'https://raw.githubusercontent.com/TheNosiriN/Babylon-Assets/master/m4a1.obj';
 
 
 var firstPerson = true;
 
 //animations
 var skeleton = null;
-var ak47 = null;
+
+// var ak47 = null;
 
 var idleAnim = null;
 var walkAnim = null;
@@ -31,7 +27,7 @@ var cameraSpeed = 0.0075;
 var walkSpeed = 0.008;
 var runSpeed = 0.05;
 var sprintSpeed = 0.008;
-var jumpSpeed = 0.0005;
+// var jumpSpeed = 0.0005;
 var jumpHeight = 10;
 var gravity = new BABYLON.Vector3(0, -0.0981, 0);
 
@@ -53,34 +49,29 @@ export default class Viewer extends Component {
 
             // SETUP CAMERA
 
-            // var camera = new BABYLON.UniversalCamera("camera", new BABYLON.Vector3.Zero(), scene);
-            // camera.inputs.clear();
-            // camera.minZ = 0;
-
-            // var hemLight = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
-            // hemLight.intensity = 0.2;
-            // hemLight.specular = BABYLON.Color3.Black();
-            // hemLight.groundColor = scene.clearColor.scale(0.75);
-
-            // var dirLight = new BABYLON.DirectionalLight("dir01", new BABYLON.Vector3(0, -0.5, -1.0), scene);
-            // dirLight.position = new BABYLON.Vector3(0, 130, 130);
-
-            // This creates and positions a free camera (non-mesh)
+            // FREE CAMERA (NON MESH)
             var camera = new BABYLON.UniversalCamera("camera", new BABYLON.Vector3.Zero(), scene);
             camera.inputs.clear();
             camera.minZ = 0;
 
+
+            // HEMLIGHT SETTINGS
             var hemLight = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
             hemLight.intensity = 0.2;
             hemLight.specular = BABYLON.Color3.Black();
             hemLight.groundColor = scene.clearColor.scale(0.75);
 
+            // DIRECTIONAL LIGHT SETTINGS
             var dirLight = new BABYLON.DirectionalLight("dir01", new BABYLON.Vector3(0, -0.5, -1.0), scene);
             dirLight.position = new BABYLON.Vector3(0, 130, 130);
 
+            // SHADOW GENERATION SETTINGS
             var shadowGenerator = new BABYLON.ShadowGenerator(3072, dirLight);
             shadowGenerator.usePercentageCloserFiltering = true;
 
+
+
+            // ENV BUILDER SETTINGS 
             var helper = scene.createDefaultEnvironment({
                 enableGroundShadow: true,
                 enableGroundMirror: true,
@@ -88,31 +79,36 @@ export default class Viewer extends Component {
                 groundSize: 100,
                 skyboxSize: 100,
             });
+
             helper.setMainColor(scene.clearColor);
             helper.groundMaterial.diffuseTexture = null;
             helper.groundMaterial.alpha = 1;
             helper.groundMaterial.fogEnabled = true;
 
+            // ADD SHAWDOWS TO CHARACTER
             var addShadows = function (mesh) {
                 mesh.receiveShadows = true;
                 shadowGenerator.addShadowCaster(mesh);
             }
 
+            // ADD MIRROR TO CHARACTER
             var addToMirror = function (mesh) {
                 helper.groundMirrorRenderList.push(mesh);
             }
 
+
+            // INITIAL INPUTS CONTROL
             const dsm = new BABYLON.DeviceSourceManager(engine);
             var deltaTime = 10;
 
-
+            // CHARACTER COMPONENTS
             var main = new BABYLON.Mesh("parent", scene);
             var target = new BABYLON.TransformNode();
             var character = new BABYLON.Mesh("character", scene);
 
 
 
-            //camera setups
+            // CAMERA SETUP
             var firstPersonCamera = {
                 middle: {
                     position: new BABYLON.Vector3(0, 1.75, 0.25),
@@ -123,42 +119,40 @@ export default class Viewer extends Component {
             };
 
 
-            var thirdPersonCamera = {
-                middle: {
-                    position: new BABYLON.Vector3(0, 1.35, -5),
-                    fov: 0.8,
-                    mouseMin: -5,
-                    mouseMax: 45
-                },
-                leftRun: {
-                    position: new BABYLON.Vector3(0.7, 1.35, -4),
-                    fov: 0.8,
-                    mouseMin: -35,
-                    mouseMax: 45
-                },
-                rightRun: {
-                    position: new BABYLON.Vector3(-0.7, 1.35, -4),
-                    fov: 0.8,
-                    mouseMin: -35,
-                    mouseMax: 45
-                },
-                far: {
-                    position: new BABYLON.Vector3(0, 1.5, -6),
-                    fov: 1.5,
-                    mouseMin: -5,
-                    mouseMax: 45
-                }
-            };
+            // var thirdPersonCamera = {
+            //     middle: {
+            //         position: new BABYLON.Vector3(0, 1.35, -5),
+            //         fov: 0.8,
+            //         mouseMin: -5,
+            //         mouseMax: 45
+            //     },
+            //     leftRun: {
+            //         position: new BABYLON.Vector3(0.7, 1.35, -4),
+            //         fov: 0.8,
+            //         mouseMin: -35,
+            //         mouseMax: 45
+            //     },
+            //     rightRun: {
+            //         position: new BABYLON.Vector3(-0.7, 1.35, -4),
+            //         fov: 0.8,
+            //         mouseMin: -35,
+            //         mouseMax: 45
+            //     },
+            //     far: {
+            //         position: new BABYLON.Vector3(0, 1.5, -6),
+            //         fov: 1.5,
+            //         mouseMin: -5,
+            //         mouseMax: 45
+            //     }
+            // };
 
+            // SWITCH FIRST TO THIRD PERSON VIEW [NEED IMPLEMENTS IF WANT TO CHANGE TO THIRD]
             function switchCamera(type) {
                 camera.position = type.position.divide(camera.parent.scaling);
                 camera.fov = type.fov;
                 mouseMin = type.mouseMin;
                 mouseMax = type.mouseMax;
             }
-
-
-
 
 
             var smallLight = new BABYLON.PointLight("boxLight", new BABYLON.Vector3.Zero(), scene);
@@ -168,7 +162,7 @@ export default class Viewer extends Component {
             smallLight.range = 5;
 
 
-            //character
+            //CHARACTER
             engine.displayLoadingUI();
 
             BABYLON.SceneLoader.ImportMesh("", "", ybotURL, scene, function (newMeshes, particleSystems, skeletons) {
@@ -180,9 +174,8 @@ export default class Viewer extends Component {
                 joints.parent = body;
                 body.parent = character;
 
-                // BABYLON.SceneLoader.ImportMesh("", "", m4URL, scene, function (newMeshes)
-                // {
-                //     m4 = newMeshes[0];
+                // BABYLON.SceneLoader.ImportMesh("", "", m4URL, scene, function (newMeshes) {
+                //     var m4 = newMeshes[0];
                 //     m4.scaling = new BABYLON.Vector3(3, 3, 3);
                 //     m4.setPivotPoint(new BABYLON.Vector3(4.5, 0.5, -2), BABYLON.Space.Local);
 
@@ -199,7 +192,7 @@ export default class Viewer extends Component {
                 // });
 
 
-
+                // SETUP MATERIAL/TEXTURE/ANIMATION FOR CHARACTER MODEL
                 body.material = new BABYLON.StandardMaterial("character", scene);
                 joints.material = new BABYLON.StandardMaterial("joints", scene);
                 body.material.diffuseColor = new BABYLON.Color3(0.81, 0.24, 0.24);
@@ -222,7 +215,7 @@ export default class Viewer extends Component {
                 sprintAnim = scene.beginWeightedAnimation(skeleton, sprintRange.from + 1, sprintRange.to, 0, true);
                 jumpAnim = scene.beginWeightedAnimation(skeleton, jumpRange.from + 10, jumpRange.to, 0, true);
 
-
+                // COLLISION DETECTION
                 main.ellipsoid = new BABYLON.Vector3(0.5, 0.9, 0.5);
                 main.ellipsoidOffset = new BABYLON.Vector3(0, main.ellipsoid.y, 0);
                 main.checkCollisions = true;
@@ -233,13 +226,15 @@ export default class Viewer extends Component {
                 character.parent = main;
                 target.parent = main;
 
-                if (firstPerson == true) {
+                // SWITCH TO DETERMINE CAMERA POSITIONING
+                if (firstPerson === true) {
                     camera.parent = character;
                     switchCamera(firstPersonCamera.middle);
-                } else {
-                    camera.parent = target;
-                    switchCamera(thirdPersonCamera.leftRun);
                 }
+                // else {
+                //     camera.parent = target;
+                //     switchCamera(thirdPersonCamera.leftRun);
+                // }
 
                 main.position = new BABYLON.Vector3(10, 0, 10);
 
@@ -248,6 +243,7 @@ export default class Viewer extends Component {
             }, function (evt) { });
 
 
+            // SETUP KEYBINDS BEFORE SCENE LOADS
             scene.registerBeforeRender(function () {
                 deltaTime = engine.getDeltaTime();
 
@@ -257,7 +253,7 @@ export default class Viewer extends Component {
                     var keyboard = dsm.getDeviceSource(BABYLON.DeviceType.Keyboard);
                     var mouse = dsm.getDeviceSource(BABYLON.DeviceType.Mouse);
                     if (keyboard) {
-                        if (firstPerson == true) {
+                        if (firstPerson === true) {
                             firstPersonMovement(
                                 keyboard.getInput(87), //W
                                 keyboard.getInput(83), //S
@@ -282,7 +278,7 @@ export default class Viewer extends Component {
 
 
 
-
+            // KEYBOARD CONTROLS
             var mouseMove = function (e) {
                 var movementX = e.movementX ||
                     e.mozMovementX ||
@@ -300,6 +296,7 @@ export default class Viewer extends Component {
             }
 
 
+            // UPDATE CAMERA 
             function updateCamera() {
                 target.rotation = lerp3(
                     target.rotation,
@@ -312,7 +309,7 @@ export default class Viewer extends Component {
 
 
 
-
+            // THIRD PERSON MOVEMENT
             function thirdPersonMovement(up, down, left, right, jump, run) {
                 var directionZ = up - down;
                 var directionX = right - left;
@@ -324,8 +321,8 @@ export default class Viewer extends Component {
 
 
                 //move
-                if (directionX != 0 || directionZ != 0) {
-                    if (run != 1) {
+                if (directionX !== 0 || directionZ !== 0) {
+                    if (run !== 1) {
                         currentState = runAnim;
                         speed = lerp(speed, runSpeed, runAnim.weight);
                     } else {
@@ -348,16 +345,16 @@ export default class Viewer extends Component {
 
 
                 //jump
-                if (jump == 1 && jumped == false) {
+                if (jump === 1 && jumped === false) {
                     jumped = true;
                 }
-                if (jumped == true) {
+                if (jumped === true) {
                     if (vsp < jumpHeight) {
                         vsp += jumpHeight * 10;
                     } else {
                         vsp += gravity.y / 10;
                         vsp = Math.min(vsp, gravity.y);
-                        if (vsp == gravity.y) {
+                        if (vsp === gravity.y) {
                             vsp = gravity.y;
                             jumped = false;
                         }
@@ -379,7 +376,7 @@ export default class Viewer extends Component {
             }
 
 
-
+            // FIRST PERSON MOVEMENT
             function firstPersonMovement(up, down, left, right, jump, run) {
                 var directionZ = up - down;
                 var directionX = right - left;
@@ -390,9 +387,9 @@ export default class Viewer extends Component {
                 var currentState = idleAnim;
 
 
-                if (directionX != 0 || directionZ != 0) {
-                    if (up == 1) {
-                        if (run != 1) {
+                if (directionX !== 0 || directionZ !== 0) {
+                    if (up === 1) {
+                        if (run !== 1) {
                             currentState = walkAnim;
                             speed = lerp(speed, walkSpeed, walkAnim.weight);
                         } else {
@@ -414,16 +411,16 @@ export default class Viewer extends Component {
                 camera.rotation.x = target.rotation.x;
 
 
-                if (jump > 0 && jumped == false) {
+                if (jump > 0 && jumped === false) {
                     jumped = true;
                 }
-                if (jumped == true) {
+                if (jumped === true) {
                     if (vsp < jumpHeight) {
                         vsp += jumpHeight / 10;
                     } else {
                         vsp += gravity.y / 10;
                         vsp = Math.min(vsp, gravity.y);
-                        if (vsp == gravity.y) {
+                        if (vsp === gravity.y) {
                             vsp = gravity.y;
                             jumped = false;
                         }
@@ -444,13 +441,13 @@ export default class Viewer extends Component {
             }
 
 
-
+            // MOVEMENT OF CHARACTER
             function switchAnimation(anim) {
                 var anims = [idleAnim, runAnim, walkAnim, sprintAnim];
 
-                if (idleAnim != undefined) {
+                if (idleAnim !== undefined) {
                     for (var i = 0; i < anims.length; i++) {
-                        if (anims[i] == anim) {
+                        if (anims[i] === anim) {
                             anims[i].weight += animationBlend * deltaTime;
                         } else {
                             anims[i].weight -= animationBlend * deltaTime;
@@ -533,11 +530,11 @@ export default class Viewer extends Component {
 
 
             // var gl = new BABYLON.GlowLayer("gl", scene);
-            // var pipeline = new BABYLON.DefaultRenderingPipeline(
-            //     "pipeline", true, scene, [camera]
-            // );
-            // pipeline.samples = 4;
-            // var ssao = new BABYLON.SSAORenderingPipeline('ssaopipeline', scene, { ssaoRatio: 0.75, combineRatio: 1.0 }, [camera]);
+            var pipeline = new BABYLON.DefaultRenderingPipeline(
+                "pipeline", true, scene, [camera]
+            );
+            pipeline.samples = 4;
+            var ssao = new BABYLON.SSAORenderingPipeline('ssaopipeline', scene, { ssaoRatio: 0.99, combineRatio: 1 }, [camera]);
             // var postProcess = new BABYLON.PostProcess("anamorphic effects", "anamorphicEffects", [], null, 1, camera);
 
 
@@ -548,7 +545,7 @@ export default class Viewer extends Component {
             var currentMesh;
 
             var getGroundPosition = function () {
-                var pickinfo = scene.pick(scene.pointerX, scene.pointerY, function (mesh) { return mesh == ground; });
+                var pickinfo = scene.pick(scene.pointerX, scene.pointerY, function (mesh) { return mesh === ground; });
                 if (pickinfo.hit) {
                     return pickinfo.pickedPoint;
                 }
@@ -598,7 +595,7 @@ export default class Viewer extends Component {
             scene.onPointerObservable.add((pointerInfo) => {
                 switch (pointerInfo.type) {
                     case BABYLON.PointerEventTypes.POINTERDOWN:
-                        if (pointerInfo.pickInfo.hit && pointerInfo.pickInfo.pickedMesh != ground) {
+                        if (pointerInfo.pickInfo.hit && pointerInfo.pickInfo.pickedMesh !== ground) {
                             pointerDown(pointerInfo.pickInfo.pickedMesh)
                         }
                         break;
